1f68b9705dd7095a45a478636032fd4871bdba0a
andrewmundellsophos <46573864+andrewmundellsophos@users.noreply.github.com>
2022-04-22 14:08:01 -0400

    Update TMAuto.py

--- TMAuto.py
+++ TMAuto.py
@@ -35,28 +35,33 @@ def marriottEmailAttachment():
     mainDLG = app['Outlook Today - Outlook']
     productDLG = mainDLG.child_window(title="Enter your product key", control_type="Window")
     productDLG.child_window(title="Close", control_type="Button").click_input()
+    time.sleep(5)
     eulaDLG = mainDLG.child_window(title="Accept the license agreement", control_type="Window")
     eulaDLG.child_window(title="Accept and start Outlook", control_type="Button").click()
+    time.sleep(5)
     mainDLG= app['Outlook Today - Outlook'] #Main application is presented as 'Outlook Today - Outlook'
     mainDLG.sophosTreeItem.click_input(double=True)#Expand the Sophos profile tree
+    time.sleep(5)
     sophosDLG = app['Sophos - Outlook']
     tmDLG=sophosDLG['z.TM AUTO'].click_input(double=True)
+    time.sleep(5)
     marriottDLG=app['z.TM AUTO - Sophos - Outlook']
     marriottDLG.child_window(title="With Attachments, Subject 100,000 BONUS Marriott Elite Points Offer, Received 1/31/2022, Size 914 KB, Flag Status Unflagged, ", control_type="DataItem").click_input()
-    time.sleep(10)
+    time.sleep(30)
     marriottDLG['BonusPointsOffer.docm776 KB1 of 1 attachmentsUse alt + down arrow to open the options menu'].click_input(double=True) #opens the attachment
+    time.sleep(10)	
 	
 #TO ENABLE THE MACRO
 def marriottMacro():
     app = Application(backend="uia").connect(title_re="^BonusPointsOffer")
     print(app.windows())
-    time.sleep(5)
+    time.sleep(10)
     mainDLG = app.window(title_re="^BonusPointsOffer")
     productKeyDLG = mainDLG.child_window(title="Enter your product key", control_type="Window")
     productKeyDLG.CloseButton.click_input()
-    time.sleep(3)
+    time.sleep(10)
     mainDLG.child_window(title="Enable Editing", control_type="Button").click_input(double=True)
-    time.sleep(5)
+    time.sleep(10)
     mainDLG.child_window(title="Enable Content", control_type="Button").click_input(double=True)
 
 if __name__ == '__main__':
