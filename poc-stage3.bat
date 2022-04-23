d5a604a1c23cf6b60222c9cc0015ddd1ad4290e1
Andrew Mundell <andrew.mundell@sophos.com>
2022-04-23 16:13:02 -0400

    AJM

--- poc-stage3.bat
+++ poc-stage3.bat
@@ -1,7 +1,3 @@
 start "" /B "C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE"
 taskkill /im outlook.exe /f
-<<<<<<< HEAD
 "C:\Program Files\Python310\python.exe" -m pip install -r c:\threat\requirements.txt
-=======
-"C:\Program Files\Python310\python.exe" -m pip install -r c:\threat\requirements.txt
->>>>>>> 0468596eacf2c78735e73befca118c42d7694e0e
