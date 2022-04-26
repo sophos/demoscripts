1dbef2897b8a6e491c1fcf93b337dc1ba91db544
Andrew Mundell <andrew.mundell@sophos.com>
2022-04-26 12:03:06 -0400

    AJM

--- CGViaKali.py
+++ CGViaKali.py
@@ -1,4 +1,3 @@
-# NEEDS CONVERTING
 from pywinauto import Application, Desktop
 import time
 def Outlook():
@@ -7,21 +6,23 @@ def Outlook():
     mainDLG=app['Outlook Today - Outlook']
     wizardDLG=mainDLG['Microsoft Office Activation Wizard2'] #there are two handles with the same child window name
     wizardDLG.child_window(title="Cancel", control_type="Button").click() #closes hardware change dialogue
-    time.sleep(5)
     mainDLG.sophosTreeItem.click_input(double=True)#Expand the Sophos profile tree
-    time.sleep(5)
     sophosDLG = app['Sophos - Outlook']
-    tmDLG=sophosDLG['z.TM AUTO'].click_input(double=True)
+    bthDLG = sophosDLG['BTH2TreeItem']
+    bthDLG.click_input(double=True)#expand the BTH2 menu tree
     time.sleep(5)
-    marriottDLG=app['z.TM AUTO - Sophos - Outlook']
-    marriottDLG.child_window(title="With Attachments, Subject 100,000 BONUS Marriott Elite Points Offer, Received 1/31/2022, Size 914 KB, Flag Status Unflagged, ", control_type="DataItem").click_input()
-    time.sleep(30)
-    marriottDLG['BonusPointsOffer.docm776 KB1 of 1 attachmentsUse alt + down arrow to open the options menu'].click_input(double=True) #opens the attachment
+    bthDLG = app['BTH2 - Sophos - Outlook']
+    bthDLG['2TreeItem'].click_input()
+    time.sleep(5)
+    twoDLG = app['2 - Sophos - Outlook'] #New window name
+    email = twoDLG.child_window(title="With Attachments, Subject Your flight has been successfully booked!, Received 1/31/2022, Size 136 KB, Flag Status Unflagged, ", control_type="DataItem")
+    email.click_input()
     time.sleep(10)
+    twoDLG['DeltaFlightItinerary.docm85 KB1 of 1 attachmentsUse alt + down arrow to open the options menu'].click_input(double=True) #opens the attachment
 def Word():
-    app = Application(backend="uia").connect(title_re="^BonusPointsOffer")
+    app = Application(backend="uia").connect(title_re="^DeltaFlightItinerary")
     time.sleep(20)
-    mainDLG = app.window(title_re="^BonusPointsOffer")
+    mainDLG = app.window(title_re="^DeltaFlightItinerary")
     wizardDLG = mainDLG['Microsoft Office Activation Wizard2'] #there are two handles with the same child window name
     wizardDLG.child_window(title="Cancel", control_type="Button").click() #closes hardware change dialogue
     time.sleep(20)
